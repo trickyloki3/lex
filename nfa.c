59da376d70fbc64711203ec7bb53b2b24b244a3a
trickyloki3 <tricky.loki3@gmail.com>
2021-05-24 00:13:17 -0400

    * Implemented node concatenation and substitution.

--- nfa.c
+++ nfa.c
@@ -63,3 +63,58 @@ static int span_push(struct lex * lex, int a, int b, int x, int y) {
 
     return 0;
 }
+
+static void node_cat(struct lex * lex, int x, int y) {
+    struct node * n;
+    struct node * o;
+
+    struct edge * e;
+    struct span * s;
+
+    n = lex->node + x;
+    o = lex->node + y;
+
+    if(o->edge) {
+        e = lex->edge + o->edge;
+        while(e->next)
+            e = lex->edge + e->next;
+
+        e->next = n->edge;
+        n->edge = o->edge;
+        o->edge = 0;
+    }
+
+    if(o->span) {
+        s = lex->span + o->span;
+        while(s->next)
+            s = lex->span + s->next;
+
+        s->next = n->span;
+        n->span = o->span;
+        o->span = 0;
+    }
+}
+
+static void node_sub(struct lex * lex, int x, int y) {
+    struct edge * e;
+    struct edge * f;
+
+    struct span * s;
+    struct span * t;
+
+    e = lex->edge + lex->edge_off;
+    f = lex->edge + lex->edge_len;
+    while(e < f) {
+        if(e->node == x)
+            e->node = y;
+        e++;
+    }
+
+    s = lex->span + lex->span_off;
+    t = lex->span + lex->span_len;
+    while(s < t) {
+        if(s->node == x)
+            s->node = y;
+        s++;
+    }
+}
--- nfa.h
+++ nfa.h
@@ -5,4 +5,7 @@ static int node_push(struct lex *, int *);
 static int edge_push(struct lex *, int, int);
 static int span_push(struct lex *, int, int, int, int);
 
+static void node_cat(struct lex *, int, int);
+static void node_sub(struct lex *, int, int);
+
 #endif
