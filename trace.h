537d40c1fe77003de704de57c8b527b2484c02fb
trickyloki3 <tricky.loki3@gmail.com>
2021-05-24 05:45:19 -0400

    * Removed extraneous semicolon for trace.

--- trace.h
+++ trace.h
@@ -1,7 +1,7 @@
 #ifndef trace_h
 #define trace_h
 
-#define trace(format, ...) panic("%s (%s:%zu): " format ".\n", __FILE__, __func__, __LINE__, ## __VA_ARGS__);
+#define trace(format, ...) panic("%s (%s:%zu): " format ".\n", __FILE__, __func__, __LINE__, ## __VA_ARGS__)
 
 static int panic(const char *, ...);
 
